1307a742d57f40e100c4e4d29091544b9c6abe61
ayudadadi <1092425124@qq.com>
2019-05-05 11:40:08 +0800

    Android材料设计之ToolBar+CardView+沉浸标题栏
    Android材料设计之FloatingActionButton+Snackbar+SheetX3
    
    Signed-off-by: ayudadadi <1092425124@qq.com>

--- demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
+++ demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
@@ -1,280 +1,77 @@
 package com.example.yls.demoa;
 
-import android.graphics.Color;
-import android.location.Location;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-import com.baidu.location.BDAbstractLocationListener;
-import com.baidu.location.BDLocation;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
-import com.baidu.mapapi.map.ArcOptions;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.BitmapDescriptor;
-import com.baidu.mapapi.map.BitmapDescriptorFactory;
-import com.baidu.mapapi.map.CircleOptions;
-import com.baidu.mapapi.map.InfoWindow;
-import com.baidu.mapapi.map.LogoPosition;
-import com.baidu.mapapi.map.MapPoi;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.Marker;
-import com.baidu.mapapi.map.MarkerOptions;
-import com.baidu.mapapi.map.MyLocationConfiguration;
-import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.map.Overlay;
-import com.baidu.mapapi.map.OverlayOptions;
-import com.baidu.mapapi.map.PolylineOptions;
-import com.baidu.mapapi.map.Stroke;
-import com.baidu.mapapi.map.TextOptions;
-import com.baidu.mapapi.model.LatLng;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    private MapView mMapView = null;
-    private BaiduMap mBaiduMap;
-    private LocationClient mLocationClient;
+    private Toolbar toolbar;
+    private CardView cardView;
+    private int count =0;
+    private FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMapView = (MapView) findViewById(R.id.bmapView);
-        mBaiduMap = mMapView.getMap();
-        mMapView.setLogoPosition(LogoPosition.logoPostionCenterTop);
-        mBaiduMap.setOnMapLongClickListener(new BaiduMap.OnMapLongClickListener() {
+        toolbar = findViewById(R.id.my_tb);
+        setSupportActionBar(toolbar);
+        floatingActionButton =findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapLongClick(LatLng latLng) {
-                Toast.makeText(MainActivity.this,"长按 纬度"+latLng.latitude
-                        +"经度"+latLng.longitude,Toast.LENGTH_SHORT
-                ).show();
-                //定义Maker坐标点
-                LatLng point = latLng;
-//构建Marker图标
-                BitmapDescriptor bitmap = BitmapDescriptorFactory
-                        .fromResource(R.drawable.maker);
-//构建MarkerOption，用于在地图上添加Marker
-                OverlayOptions option = new MarkerOptions()
-                        .title("天河客运站")
-                        .position(point)
-                        .icon(bitmap);
-//在地图上添加Marker，并显示
-                mBaiduMap.addOverlay(option);
+            public void onClick(View view) {
+                Snackbar.make(floatingActionButton, "Hello Snack", Snackbar.LENGTH_LONG)
+                        .setAction("点击", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "Hi , I'm toly", Toast.LENGTH_SHORT).show();
 
-                /*绘制折线*/
-                /*drawMPloyline(latLng);*/
-                /*绘制长方形*/
-                /*drawRectangle(latLng);*/
-                /*绘制弧线*/
-                /*drawArc(latLng);*/
-                /*绘制圆*/
-                /*drawCircle(latLng);*/
-                /*添加文字*/
-                /*drawText(latLng);*/
-                /*绘制信息框*/
-                drawInfoWindow(latLng);
-
+                            }
+                        }).show();
             }
         });
-        mBaiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
-
-            public boolean onMarkerClick(Marker marker) {
-                Toast.makeText(MainActivity.this,marker.getTitle(),Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-        mBaiduMap.setOnMapDoubleClickListener(new BaiduMap.OnMapDoubleClickListener() {
+        cardView = findViewById(R.id.id_cv);
+        cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapDoubleClick(LatLng latLng) {
-                Toast.makeText(MainActivity.this,"双击 纬度"+latLng.latitude
-                        +"经度"+latLng.longitude,Toast.LENGTH_SHORT
-                ).show();
-
-            }
-        });
-        mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                Toast.makeText(MainActivity.this,"单击 纬度"+latLng.latitude
-                        +"经度"+latLng.longitude,Toast.LENGTH_SHORT
-                ).show();
-            }
-
-            @Override
-            public boolean onMapPoiClick(MapPoi mapPoi) {
-                Toast.makeText(MainActivity.this,"MapPoi单击 纬度"+mapPoi.getName()
-                        +"坐标"+mapPoi.getPosition().toString(),Toast.LENGTH_SHORT
-                ).show();
-                return false;
+            public void onClick(View view) {
+                runAnima();
             }
         });
 
-//普通地图 ,mBaiduMap是地图控制器对象
-        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
-        mBaiduMap.setMyLocationEnabled(true);
-
-        MyLocationConfiguration myLocationConiguration = new MyLocationConfiguration(
-                MyLocationConfiguration.LocationMode.FOLLOWING,
-        true,
-                BitmapDescriptorFactory.fromResource(R.drawable.tubiao),
-        0xAAFFFF88,
-        0xAA00FF00);
-
-        mBaiduMap.setMyLocationConfiguration(myLocationConiguration);
-        //定位初始化
-        mLocationClient = new LocationClient(this);
-
-//通过LocationClientOption设置LocationClient相关参数
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
-        option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setScanSpan(1000);
-
-//设置locationClientOption
-        mLocationClient.setLocOption(option);
-
-//注册LocationListener监听器
-        MyLocationListener myLocationListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(myLocationListener);
-//开启地图定位图层
-        mLocationClient.start();
     }
-
-    private void drawInfoWindow(LatLng latLng) {
-//用来构造InfoWindow的Button
-        Button button = new Button(getApplicationContext());
-        button.setBackgroundResource(R.drawable.word);
-        button.setText("InfoWindow");
-
-//构造InfoWindow
-//point 描述的位置点
-//-100 InfoWindow相对于point在y轴的偏移量
-        InfoWindow mInfoWindow = new InfoWindow(button, latLng, -50);
-
-//使InfoWindow生效
-        mBaiduMap.showInfoWindow(mInfoWindow);
-    }
-
-    private void drawText(LatLng latLng) {
-        //文字覆盖物位置坐标
-        LatLng llText = latLng;
-
-//构建TextOptions对象
-        OverlayOptions mTextOptions = new TextOptions()
-                .text("我爱广交") //文字内容
-                .bgColor(0xAAFFFF00) //背景色
-                .fontSize(24) //字号
-                .fontColor(0xFFFF00FF) //文字颜色
-                .rotate(-30) //旋转角度
-                .position(llText);
-
-//在地图上显示文字覆盖物
-        Overlay mText = mBaiduMap.addOverlay(mTextOptions);
-    }
-
-    private void drawCircle(LatLng latLng) {
-//圆心位置
-        LatLng center = latLng;
-
-//构造CircleOptions对象
-        CircleOptions mCircleOptions = new CircleOptions().center(center)
-                .radius(5000)
-                .fillColor(0xAA0000FF) //填充颜色
-                .stroke(new Stroke(5, 0xAA00ff00)); //边框宽和边框颜色
-
-//在地图上显示圆
-        Overlay mCircle = mBaiduMap.addOverlay(mCircleOptions);
-    }
-
-    private void drawArc(LatLng latLng) {
-        // 添加弧线坐标数据
-        LatLng p1 = latLng;
-        LatLng p2 = new LatLng(p1.latitude-0.08, p1.longitude+0.08);//中间点
-        LatLng p3 = new LatLng(p1.latitude, p1.longitude+0.16);//终点
-
-//构造ArcOptions对象
-        OverlayOptions mArcOptions = new ArcOptions()
-                .color(Color.RED)
-                .width(10)
-                .points(p1, p2, p3);
-
-//在地图上显示弧线
-        Overlay mArc = mBaiduMap.addOverlay(mArcOptions);
-    }
-
-    private void drawRectangle(LatLng latLng) {
-        LatLng p1 = latLng;
-        LatLng p2 = new LatLng(p1.latitude-0.05,p1.longitude);
-        LatLng p3 = new LatLng(p2.latitude,p2.longitude+0.05);
-        LatLng p4 = new LatLng(p3.latitude+0.05,p3.longitude);
-        List<LatLng> points = new ArrayList<LatLng>();
-        points.add(p1);
-        points.add(p2);
-        points.add(p3);
-        points.add(p4);
-        points.add(p1);
-        //设置折线的属性
-        OverlayOptions mOverlayOptions = new PolylineOptions()
-                .width(10)
-                .color(0xAAFF0000)
-                .points(points);
-//在地图上绘制折线
-//mPloyline 折线对象
-        Overlay mPolyline = mBaiduMap.addOverlay(mOverlayOptions);
-    }
-
-    private void drawMPloyline(LatLng latLng) {
-        LatLng p1 = latLng;
-        LatLng p2 = new LatLng(p1.latitude-0.005,p1.longitude+0.005);
-        LatLng p3 = new LatLng(p1.latitude,p1.longitude+0.01);
-        List<LatLng> points = new ArrayList<LatLng>();
-        points.add(p1);
-        points.add(p2);
-        points.add(p3);
-        //设置折线的属性
-        OverlayOptions mOverlayOptions = new PolylineOptions()
-                .width(10)
-                .color(0xAAFF0000)
-                .points(points);
-//在地图上绘制折线
-//mPloyline 折线对象
-        Overlay mPolyline = mBaiduMap.addOverlay(mOverlayOptions);
-    }
-
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
+    private void runAnima() {
+        cardView.animate().translationX(100 + count).setDuration(1000).start();
+        count += 10;
     }
     @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
     }
     @Override
-    protected void onDestroy() {
-        mLocationClient.stop();
-        mBaiduMap.setMyLocationEnabled(false);
-        mMapView.onDestroy();
-        mMapView = null;
-        super.onDestroy();
-    }
-    public class MyLocationListener extends BDAbstractLocationListener {
-        public void onReceiveLocation(BDLocation location) {
-            //mapView 销毁后不在处理新接收的位置
-            if (location == null || mMapView == null){
-                return;
-            }
-            MyLocationData locData = new MyLocationData.Builder()
-                    .accuracy(location.getRadius())
-                    // 此处设置开发者获取到的方向信息，顺时针0-360
-                    .direction(location.getDirection()).latitude(location.getLatitude())
-                    .longitude(location.getLongitude()).build();
-            mBaiduMap.setMyLocationData(locData);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.tab_add:
+                Toast.makeText(MainActivity.this, "tab_add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tab_about:
+                Toast.makeText(MainActivity.this, "about",Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.tab_before:
+                Toast.makeText(MainActivity.this, "before",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                finish();
+                break;
         }
+        return super.onOptionsItemSelected(item);
     }
 }

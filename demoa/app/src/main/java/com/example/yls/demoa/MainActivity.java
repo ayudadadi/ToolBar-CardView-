5a7a06a0aa34df35fc9807ce07e0c2be8d210241
ayudadadi <1092425124@qq.com>
2019-05-05 11:34:02 +0800

    Signed-off-by: ayudadadi <1092425124@qq.com>

--- demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
+++ demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
@@ -4,12 +4,6 @@ import android.graphics.Color;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -36,51 +30,14 @@ import com.baidu.mapapi.map.PolylineOptions;
 import com.baidu.mapapi.map.Stroke;
 import com.baidu.mapapi.map.TextOptions;
 import com.baidu.mapapi.model.LatLng;
-import com.baidu.mapapi.model.LatLngBounds;
-import com.baidu.mapapi.search.busline.BusLineResult;
-import com.baidu.mapapi.search.busline.BusLineSearch;
-import com.baidu.mapapi.search.busline.BusLineSearchOption;
-import com.baidu.mapapi.search.busline.OnGetBusLineSearchResultListener;
-import com.baidu.mapapi.search.core.PoiInfo;
-import com.baidu.mapapi.search.core.SearchResult;
-import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
-import com.baidu.mapapi.search.poi.PoiBoundSearchOption;
-import com.baidu.mapapi.search.poi.PoiCitySearchOption;
-import com.baidu.mapapi.search.poi.PoiDetailResult;
-import com.baidu.mapapi.search.poi.PoiDetailSearchOption;
-import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
-import com.baidu.mapapi.search.poi.PoiIndoorResult;
-import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
-import com.baidu.mapapi.search.poi.PoiResult;
-import com.baidu.mapapi.search.poi.PoiSearch;
-import com.baidu.mapapi.search.route.BikingRouteResult;
-import com.baidu.mapapi.search.route.DrivingRouteResult;
-import com.baidu.mapapi.search.route.IndoorRouteResult;
-import com.baidu.mapapi.search.route.MassTransitRouteResult;
-import com.baidu.mapapi.search.route.OnGetRoutePlanResultListener;
-import com.baidu.mapapi.search.route.PlanNode;
-import com.baidu.mapapi.search.route.RoutePlanSearch;
-import com.baidu.mapapi.search.route.TransitRouteResult;
-import com.baidu.mapapi.search.route.WalkingRoutePlanOption;
-import com.baidu.mapapi.search.route.WalkingRouteResult;
-import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
-import com.baidu.mapapi.search.sug.SuggestionResult;
-import com.baidu.mapapi.search.sug.SuggestionSearch;
-import com.baidu.mapapi.search.sug.SuggestionSearchOption;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     private MapView mMapView = null;
     private BaiduMap mBaiduMap;
     private LocationClient mLocationClient;
-    private LatLng mlatLng;
-    private AutoCompleteTextView auto_text;
-    private SuggestionSearch suggestionSearch;
-    private String key;
-    private TextWatcher textWatcher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,208 +45,9 @@ public class MainActivity extends AppCompatActivity {
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
         mMapView.setLogoPosition(LogoPosition.logoPostionCenterTop);
-
-        findViewById(R.id.b8).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
-        findViewById(R.id.b7).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RoutePlanSearch routePlanSearch = RoutePlanSearch.newInstance();
-                routePlanSearch.setOnGetRoutePlanResultListener(new OnGetRoutePlanResultListener() {
-                    @Override
-                    public void onGetWalkingRouteResult(WalkingRouteResult walkingRouteResult) {
-
-                    }
-
-                    @Override
-                    public void onGetTransitRouteResult(TransitRouteResult transitRouteResult) {
-
-                    }
-
-                    @Override
-                    public void onGetMassTransitRouteResult(MassTransitRouteResult massTransitRouteResult) {
-
-                    }
-
-                    @Override
-                    public void onGetDrivingRouteResult(DrivingRouteResult drivingRouteResult) {
-
-                    }
-
-                    @Override
-                    public void onGetIndoorRouteResult(IndoorRouteResult indoorRouteResult) {
-
-                    }
-
-                    @Override
-                    public void onGetBikingRouteResult(BikingRouteResult bikingRouteResult) {
-
-                    }
-                });
-                PlanNode start = PlanNode.withCityNameAndPlaceName("广州","广东交通职业技术学院");
-                PlanNode end = PlanNode.withCityNameAndPlaceName("广州","广州塔");
-                routePlanSearch.walkingSearch(new WalkingRoutePlanOption().from(start).to(end));
-
-            }
-        });
-
-
-        findViewById(R.id.b6).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BusLineSearch busLineSearch = BusLineSearch.newInstance();
-                busLineSearch.setOnGetBusLineSearchResultListener(new OnGetBusLineSearchResultListener() {
-                    @Override
-                    public void onGetBusLineResult(BusLineResult busLineResult) {
-                        Log.e(TAG,busLineResult.toString());
-
-                    }
-                });
-                busLineSearch.searchBusLine(new BusLineSearchOption().city("广州")
-                .uid("aa82413e7f6bf160f5b91651"));
-            }
-        });
-
-
-
-        auto_text =findViewById(R.id.auto_text);
-        findViewById(R.id.b5).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-        suggestionSearch = SuggestionSearch.newInstance();
-        suggestionSearch.setOnGetSuggestionResultListener(new OnGetSuggestionResultListener() {
-            @Override
-            public void onGetSuggestionResult(SuggestionResult suggestionResult) {
-                List<SuggestionResult.SuggestionInfo> suggestions = suggestionResult.getAllSuggestions();
-                final ArrayList<String> strList = new ArrayList<>();
-                for (int i =0;i<suggestions.size();i++){
-                    Log.e(TAG,suggestions.get(i).toString());
-                    strList.add(suggestions.get(i).getKey());
-                }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                        android.R.layout.simple_list_item_1,strList);
-                auto_text.setAdapter(adapter);
-                auto_text.removeTextChangedListener(textWatcher);
-                auto_text.setText(key);
-                auto_text.setSelection(key.length());
-                auto_text.addTextChangedListener(textWatcher);
-
-            }
-        });
-
-        textWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                key = editable.toString();
-                if(key.length()==0){
-                    return;
-                }
-                String key = editable.toString();
-                suggestionSearch.requestSuggestion(new SuggestionSearchOption().
-                        city("广州").keyword("交通"));
-
-            }
-        };
-
-        final PoiSearch poiSearch = PoiSearch.newInstance();
-        poiSearch.setOnGetPoiSearchResultListener(new OnGetPoiSearchResultListener() {
-            @Override
-            public void onGetPoiResult(PoiResult poiResult) {
-                List<PoiInfo> poiList =poiResult.getAllPoi();
-                for(int i =0;i<poiList.size();i++){
-                    Log.e(TAG,poiList.get(i).toString());
-                }
-
-
-                    //创建PoiOverlay对象
-                    PoiOverlay poiOverlay = new PoiOverlay(mBaiduMap);
-
-                    //设置Poi检索数据
-                    poiOverlay.setData(poiResult);
-
-                    //将poiOverlay添加至地图并缩放至合适级别
-                    poiOverlay.addToMap();
-                    poiOverlay.zoomToSpan();
-
-            }
-
-            @Override
-            public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
-
-            }
-
-            @Override
-            public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
-                Log.e(TAG,poiDetailSearchResult.getPoiDetailInfoList().get(0).toString());
-            }
-
-            @Override
-            public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
-
-            }
-        });
-        findViewById(R.id.b1).setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                poiSearch.searchInCity(new PoiCitySearchOption().city("广州").keyword("84路"));
-
-            }
-        });
-        findViewById(R.id.b2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /**
-                 * 设置矩形检索区域
-                 */
-                LatLngBounds searchBounds = new LatLngBounds.Builder()
-                        .include(new LatLng( mlatLng.latitude+0.05, mlatLng.latitude-0.05))
-                        .include(new LatLng( mlatLng.latitude-0.05, mlatLng.latitude+0.05))
-                        .build();
-                /**
-                 * 在searchBounds区域内检索餐厅
-                 */
-                poiSearch.searchInBound(new PoiBoundSearchOption()
-                        .bound(searchBounds)
-                        .keyword("学校"));
-            }
-        });
-        findViewById(R.id.b3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                poiSearch.searchNearby(new PoiNearbySearchOption().location(mlatLng)
-                .keyword("美食").radius(1000));
-            }
-        });
-        findViewById(R.id.b4).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                poiSearch.searchPoiDetail(new PoiDetailSearchOption().poiUids("9fcc825914e5bbe303ec0137"));
-            }
-        });
-
         mBaiduMap.setOnMapLongClickListener(new BaiduMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                mlatLng = latLng;
                 Toast.makeText(MainActivity.this,"长按 纬度"+latLng.latitude
                         +"经度"+latLng.longitude,Toast.LENGTH_SHORT
                 ).show();
@@ -317,12 +75,10 @@ public class MainActivity extends AppCompatActivity {
                 /*添加文字*/
                 /*drawText(latLng);*/
                 /*绘制信息框*/
-                /*drawInfoWindow(latLng);*/
+                drawInfoWindow(latLng);
 
             }
         });
-
-
         mBaiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
 
             public boolean onMarkerClick(Marker marker) {

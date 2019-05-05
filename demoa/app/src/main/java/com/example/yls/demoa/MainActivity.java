943b7cced0466d868daf8fb143c2428d7610e207
ayudadadi <1092425124@qq.com>
2019-05-05 10:53:03 +0800

    实现 CollapsingToolbarLayout+Palette DrawerLayout+NavigationView+TextInputLayout
    Signed-off-by: ayudadadi <1092425124@qq.com>

--- demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
+++ demoa/app/src/main/java/com/example/yls/demoa/MainActivity.java
@@ -1,65 +1,32 @@
 package com.example.yls.demoa;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.support.v7.graphics.Palette;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final  int MSG_REFRESH = 1001;
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private ListView listView;
-    private ArrayList<String> data = new ArrayList<>();
-    private ArrayAdapter adapter;
-    private Handler handler;
-
+    private ImageView iv_header;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initHandler();
-        swipeRefreshLayout = findViewById(R.id.swipeLayout);
-        listView = findViewById(R.id.mylist);
-        data.add("软件");
-        adapter =new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,data);
-        listView.setAdapter(adapter);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        collapsingToolbarLayout = findViewById(R.id.ctl_toolbar);
+        iv_header = findViewById(R.id.iv_header);
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) iv_header.getDrawable();
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
             @Override
-            public void onRefresh() {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Thread.sleep(3000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        for (int i =0;i<10;i++){
-                            data.add("下拉刷新"+i);
-                        }
-                        handler.sendEmptyMessage(MSG_REFRESH);
-                    }
-                }).start();
+            public void onGenerated(Palette palette) {
+                int color = palette.getDarkMutedColor(Color.RED);
+                collapsingToolbarLayout.setContentScrimColor(color);
             }
         });
-    }
 
-    private void initHandler() {
-        handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message message) {
-                if(message.what ==MSG_REFRESH){
-                    adapter.notifyDataSetChanged();
-                    swipeRefreshLayout.setRefreshing(false);
-                }
-                return false;
-            }
-        });
     }
 }
